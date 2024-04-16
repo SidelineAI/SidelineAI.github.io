b2ed07abdaa2b109a75ed08bf10fbe648eb5bd94
Ayush Pai <ayushpai@gmail.com>
2024-04-16 02:24:41 -0400

    fixed error with deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c1b69314b43be0a"; }
+/******/ 		__webpack_require__.h = function() { return "89a22c945b4db444"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import { Button, Divider, Input, Text } from '@nextui-org/react';
+import { Button, Divider, FormElement, Input, Text } from '@nextui-org/react';
 import { Box } from '../styles/box';
 import { Flex } from '../styles/flex';
 import { db } from './firebase-config';
 import { collection, addDoc } from 'firebase/firestore';
 
-export const Hero = () => {
-   const [email, setEmail] = useState('');
+export const Hero: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
 
-   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (event: React.ChangeEvent<FormElement>): void => {
       setEmail(event.target.value);
-   };
+  };
 
-   const handleJoinWaitlist = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const handleJoinWaitlist = async (event: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
         //event.preventDefault(); // Prevent the form from refreshing the page
         // Basic email validation
         if (email.match(/^\S+@\S+\.\S+$/)) {
@@ -23,96 +23,97 @@ export const Hero = () => {
                 });
                 console.log("Document written with ID: ", docRef.id);
                 alert('Thanks for joining the waitlist!');
+                setEmail(''); // Clear input after submission
             } catch (error) {
                 console.error("Error adding document: ", error);
-                alert(error);
+                alert('Failed to join the waitlist. Please try again.');
             }
         } else {
             alert('Please enter a valid email address.');
         }
     };
 
-   return (
-      <>
-         <Flex
-            css={{
-               gap: '$3',
-               px: '$6',
-               flexDirection: 'column',
-               alignContent: 'center',
-               justifyContent: 'center',
-               alignItems: 'center',
-               width: '100%',
-               marginBottom: '100px', // Added marginBottom here
-               '@sm': {
-                  flexDirection: 'row',
-                  mt: '$20',
-               },
-            }}
-            justify={'center'}
-         >
-            <Box
-               css={{
-                  pt: '$13',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '$5',
-               }}
+    return (
+        <>
+            <Flex
+                css={{
+                    gap: '$3',
+                    px: '$6',
+                    flexDirection: 'column',
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100%',
+                    marginBottom: '100px', // Added marginBottom here
+                    '@sm': {
+                        flexDirection: 'row',
+                        mt: '$20',
+                    },
+                }}
+                justify={'center'}
             >
-               <Box
-                  css={{
-                     maxWidth: '600px',
-                  }}
-               >
-                  <Text
-                     h1
-                     css={{
-                        display: 'inline',
-                        fontSize: '90px',
-                     }}
-                  >  
-                     Side<span style={{ color: '#C9082A' }}>l</span>ine
-                  </Text>
-               </Box>
+                <Box
+                    css={{
+                        pt: '$13',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '$5',
+                    }}
+                >
+                    <Box
+                        css={{
+                            maxWidth: '600px',
+                        }}
+                    >
+                        <Text
+                            h1
+                            css={{
+                                display: 'inline',
+                                fontSize: '90px',
+                            }}
+                        >  
+                            Sideline
+                        </Text>
+                    </Box>
 
-               <Text
-                  css={{
-                     color: '$accents8',
-                     maxWidth: '400px',
-                  }}
-                  size={'$lg'}
-                  span
-               >
-                  All-in-one search platform for sports scouting and coaching.
-               </Text>
+                    <Text
+                        css={{
+                            color: '$accents8',
+                            maxWidth: '400px',
+                        }}
+                        size={'$lg'}
+                        span
+                    >
+                        All-in-one search platform for sports scouting and coaching.
+                    </Text>
 
-               <Flex
-                  css={{
-                     gap: '$8',
-                     pt: '$4',
-                  }}
-                  wrap={'wrap'}
-               >
-                  <Input placeholder="Enter your email address" size="lg" value={email} onChange={handleInputChange} />
-                  <Button css={{ backgroundColor: '#C9082A', color: 'white' }} onClick={handleJoinWaitlist}>
-                     Join Waitlist
-                  </Button>
-               </Flex>
-            </Box>
-            <Box
-               css={{
-                  '& img': {
-                     width: '775px',
-                     objectFit: 'contain',
-                  },
-               }}
-            >
-               <img src="sideline.png" />
-            </Box>
-         </Flex>
-         <Divider
-            css={{position: 'absolute', inset: '0p', left: '0', mt: '$10'}}
-         />
-      </>
-   );
+                    <Flex
+                        css={{
+                            gap: '$8',
+                            pt: '$4',
+                        }}
+                        wrap={'wrap'}
+                    >
+                        <Input placeholder="Enter your email address" size="lg" value={email} onChange={handleInputChange} />
+                        <Button css={{ backgroundColor: '#C9082A', color: 'white' }} onClick={handleJoinWaitlist}>
+                            Join Waitlist
+                        </Button>
+                    </Flex>
+                </Box>
+                <Box
+                    css={{
+                        '& img': {
+                            width: '775px',
+                            objectFit: 'contain',
+                        },
+                    }}
+                >
+                    <img src="sideline.png" />
+                </Box>
+            </Flex>
+            <Divider
+                css={{position: 'absolute', inset: '0p', left: '0', mt: '$10'}}
+            />
+        </>
+    );
 };
